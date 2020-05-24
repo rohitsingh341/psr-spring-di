186d30f6739fc6db69b18498862f4f83c9f228ac
rohitsingh <rohitsingh341@gmail.com>
2020-05-24 18:52:59 +0530

    Adding Maven Project files

---
+++ psr-spring-di/src/main/java/com/sprngframework/psrspringdi/PsrSpringDiApplication.java
@@ -0,0 +1,13 @@
+package com.sprngframework.psrspringdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsrSpringDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsrSpringDiApplication.class, args);
+	}
+
+}
---
+++ psr-spring-di/src/test/java/com/sprngframework/psrspringdi/PsrSpringDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprngframework.psrspringdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsrSpringDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
